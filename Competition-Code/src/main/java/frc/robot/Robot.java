7afb9584697db65b4eb6baf66748a147186d2cea
RoboDilbert <Knackrobotics@gmail.com>
2020-01-07 19:37:14 -0600

    added test driver code

--- Competition-Code/src/main/java/frc/robot/Robot.java
+++ Competition-Code/src/main/java/frc/robot/Robot.java
@@ -7,9 +7,17 @@
 
 package frc.robot;
 
+import com.kauailabs.navx.frc.AHRS;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import com.revrobotics.CANEncoder;
+import com.revrobotics.CANSparkMax;
+import edu.wpi.first.wpilibj.drive.MecanumDrive;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.Joystick;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import edu.wpi.first.wpilibj.SPI;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -24,6 +32,29 @@ public class Robot extends TimedRobot {
   private String m_autoSelected;
   private final SendableChooser<String> m_chooser = new SendableChooser<>();
 
+  AHRS ahrs;
+  private MecanumDrive m_myRobot;
+  private Joystick m_leftStick;
+  private CANSparkMax m_leftFrontMotor;
+  private CANSparkMax m_leftBackMotor;
+  private CANSparkMax m_rightFrontMotor;
+  private CANSparkMax m_rightBackMotor;
+  private static final int leftFrontDeviceID = 1; 
+  private static final int leftBackDeviceID = 3;
+  private static final int rightFrontDeviceID = 5;
+  private static final int rightBackDeviceID = 2;
+  private CANEncoder m_leftBackEncoder;
+  private CANEncoder m_leftFrontEncoder;                                                                                 
+  private CANEncoder m_rightBackEncoder;
+  private CANEncoder m_rightFrontEncoder;
+  public double yValue;
+  public double xValue;
+  public double zValue;
+  public float leftPower;
+  public float rightPower;
+  public double roboGyro; 
+
+
   /**
    * This function is run when the robot is first started up and should be
    * used for any initialization code.
@@ -33,9 +64,33 @@ public class Robot extends TimedRobot {
     m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
     m_chooser.addOption("My Auto", kCustomAuto);
     SmartDashboard.putData("Auto choices", m_chooser);
+
+
+    m_leftFrontMotor = new CANSparkMax(leftFrontDeviceID, MotorType.kBrushless);
+    m_leftBackMotor = new CANSparkMax(leftBackDeviceID, MotorType.kBrushless);
+    m_rightFrontMotor = new CANSparkMax(rightFrontDeviceID, MotorType.kBrushless);
+    m_rightBackMotor = new CANSparkMax(rightBackDeviceID, MotorType.kBrushless);
+    m_myRobot = new MecanumDrive(m_leftFrontMotor, m_leftBackMotor, m_rightFrontMotor, m_rightBackMotor);
+
+    m_leftFrontEncoder = m_leftFrontMotor.getEncoder();
+    m_leftBackEncoder = m_leftBackMotor.getEncoder();
+    m_rightFrontEncoder = m_rightFrontMotor.getEncoder();
+    m_rightBackEncoder = m_rightBackMotor.getEncoder();
+
+    try {
+      ahrs = new AHRS(SPI.Port.kMXP);
+    } catch (RuntimeException ex ) {
+      DriverStation.reportError("Error instantiating navX MXP:  " + ex.getMessage(), true);
+    }
+    
+    m_leftStick = new Joystick(1); 
   }
 
+
+  
   /**
+   * 
+   * 
    * This function is called every robot packet, no matter the mode. Use
    * this for items like diagnostics that you want ran during disabled,
    * autonomous, teleoperated and test.
@@ -46,6 +101,8 @@ public class Robot extends TimedRobot {
   @Override
   public void robotPeriodic() {
   }
+
+
 
   /**
    * This autonomous (along with the chooser code above) shows how to select
@@ -86,6 +143,33 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void teleopPeriodic() {
+    yValue = m_leftStick.getY();
+    xValue = m_leftStick.getX();
+    zValue = m_leftStick.getZ();
+
+    xValue = xValue/2;
+    yValue = yValue/2;
+    zValue = zValue/3;
+
+    if(xValue < 0.1 && xValue > -0.1){
+      xValue = 0;
+    }
+    if(yValue < 0.1 && yValue > -0.1){
+      yValue = 0;
+    }
+    if(zValue < 0.1 && zValue > -0.1){
+      zValue = 0;
+    }
+
+    roboGyro = ahrs.getAngle();
+
+
+    
+    m_myRobot.driveCartesian(-xValue, yValue, zValue, -roboGyro);
+
+    if (m_leftStick.getRawButton(3)){
+      ahrs.reset();
+    }
   }
 
   /**
