a1e4c26ae8685ad7be91df3e718cb377f894d9f5
Sam Bradin <sambradin@Sams-MacBook.local>
2020-01-28 19:22:02 -0600

    More shooter

--- Competition-Code/src/main/java/frc/robot/subsystems/Shooter.java
+++ Competition-Code/src/main/java/frc/robot/subsystems/Shooter.java
@@ -4,6 +4,8 @@ import com.revrobotics.CANEncoder;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.SpeedController;
 import frc.robot.util.*;
 public class Shooter{
 
@@ -11,22 +13,30 @@ public class Shooter{
     
     public static final CANSparkMax leftShooter = new CANSparkMax(Constants.leftShooterDeviceID, MotorType.kBrushless);
     public static final CANSparkMax rightShooter = new CANSparkMax(Constants.rightShooterDeviceID, MotorType.kBrushless);
-    public static CANEncoder leftShooterEncoder;
-    public static CANEncoder rightShooterEncoder;
 
     
-
+    public static enum ShooterDirections{
+        FORWARD, REVERSE, STOP, DUNNO;
+    }
    
     
     public static void init(){
         
         leftShooter.setIdleMode(IdleMode.kCoast);
         rightShooter.setIdleMode(IdleMode.kCoast);
-        leftShooterEncoder = leftShooter.getEncoder();
-        rightShooterEncoder = rightShooter.getEncoder();
-    }
-
-    public static void run(){
+        rightShooter.setInverted(true);
         
     }
+
+    public static void ShooterControl(SpeedController controller1, SpeedController controller2, ShooterDirections value){
+        if(value == ShooterDirections.FORWARD){
+            controller1.set(0.8);
+            controller2.set(0.8);
+        }
+
+        else if( true){
+
+        }
+    }
+    
 }
